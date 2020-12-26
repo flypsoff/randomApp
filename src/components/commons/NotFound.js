e1586ea92be34612aa4b18169dded3535b1a6048
Vlad Sevostianov <flypsoff@gmail.com>
2020-12-26 15:13:29 +0200

    for validation page

---
+++ src/components/commons/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NotFound = (props) => {
+    return (
+        <div>  
+            {props.children}
+        </div>
+    )
+}
+
+export default NotFound
---
+++ src/components/commons/Preloader.js
@@ -0,0 +1,11 @@
+import preloader from '../../assets/img/preloader.gif'
+
+const Preloader = () => {
+    return (
+        <div>
+            <img src={preloader} alt='preloader' />
+        </div>
+    )
+}
+
+export default Preloader

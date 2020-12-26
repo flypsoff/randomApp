2d4c70e69a6b8490ab6d1c610eae56e9bf6127cb
Vlad Sevostianov <flypsoff@gmail.com>
2020-12-26 15:16:55 +0200

    added some cases start_, finish_ loading, and current_car, current_car_error

--- src/reducers/shops.js
+++ src/reducers/shops.js
@@ -1,11 +1,21 @@
 import {
     GET_BRANDS,
     GET_CARS,
+    SHOP_LOADING_START,
+    SHOP_LOADING_FINISH,
+    GET_CURRENT_CAR,
+    GET_CURRENT_CAR_ERROR
 } from '../constants'
 
 let initialShopState = {
     brands: [],
     cars: [],
+    currentCar: {},
+    currentCarError: {
+        title: '',
+        message: ''
+    },
+    loading: false
 }
 
 const shops = (state = initialShopState, action) => {
@@ -19,6 +29,28 @@ const shops = (state = initialShopState, action) => {
             return {
                 ...state,
                 brands: action.brands
+            }
+        case SHOP_LOADING_START:
+            return {
+                ...state,
+                currentCar: {},
+                loading: true
+            }
+        case SHOP_LOADING_FINISH:
+            return {
+                ...state,
+                loading: false
+            }
+        case GET_CURRENT_CAR:
+            return {
+                ...state,
+                currentCar: {...action.currentCar},
+                currentCarError: {title: '', message: ''}
+            }
+        case GET_CURRENT_CAR_ERROR:
+            return {
+                ...state,
+                currentCarError: action.error
             }
         default:
             return state

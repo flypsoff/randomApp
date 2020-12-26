76a4698dcb73c955d71d4051acd5aad63dc92085
Vlad Sevostianov <flypsoff@gmail.com>
2020-12-26 15:20:11 +0200

    using React.lazy and HOC with Suspense

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+
 import Header from './components/Header/Header'
 import Home from './components/Home/Home'
-import TodoContainer from './container/TodoContainer'
 import History from './components/Todo/History/History'
-import { BrowserRouter, Route, Switch, Redirect  } from 'react-router-dom'
-import ContactUsContainer from './container/ContactUsContainer'
-import ShopContainer from './container/ShopContainer'
+
+import TodoContainer from './container/TodoContainer'
+import CarContainer from './container/CarContainer'
+
+import withSuspense from './hoc/withSuspense'
+
+const ContactUsContainer = React.lazy(() => import('./container/ContactUsContainer'))
+const ShopContainer = React.lazy(() => import('./container/ShopContainer'))
 
 function App() {
   return (
@@ -13,9 +20,11 @@ function App() {
       <Switch>
         <Route exact path={'/'} render={() => <Home />} />
         <Route exact path={'/todo'} render={() => <TodoContainer />} />
-        <Route exact path={'/todo/history'} render={() => <History />}/>
-        <Route exact path={'/shop/:brand?'} render={() => <ShopContainer />} />
-        <Route path={'/contactus'} render={() => <ContactUsContainer />} />
+        <Route exact path={'/todo/history'} render={() => <History />} />
+        <Route exact path={'/car/:id?'} render={withSuspense(CarContainer)} />
+        <Route exact path={'/shop/:brand?'} render={withSuspense(ShopContainer)} />
+        <Route path={'/contactus'} render={withSuspense(ContactUsContainer)} />
+
         <Redirect from={'/'} to={'/'} />
       </Switch>
     </BrowserRouter>

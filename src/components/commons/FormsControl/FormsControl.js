a3b61e0e0708714f124cf98ca5bb4a88e86de5dc
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-11 19:32:13 +0200

    changed InputRender for form

--- src/components/commons/FormsControl/FormsControl.js
+++ src/components/commons/FormsControl/FormsControl.js
@@ -1,25 +1,8 @@
 import React from 'react'
 
-export const InputRender = ({ input, meta, label, ...props }) => {
+export const InputRender = ({ input, meta, label, placeholder, ...props }) => {
     return (
-        <div>
-            <label>{label}</label>
-            <div>
-                <input {...input} {...props} placeholder={label} />
-            </div>
-            {meta.touched && meta.error && <span style={{ color: 'darkred' }}>{meta.error}</span>}
-        </div>
+        <input {...input} {...props} placeholder={placeholder} />
     )
 }
 
-export const TextareaRender = ({ input, meta, label, ...props }) => {
-    return (
-        <div>
-            <label>{label}</label>
-            <div>
-                <textarea {...input} {...props} placeholder={label} />
-            </div>
-            {meta.touched && meta.error && <span style={{ color: 'darkred' }}>{meta.error}</span>}
-        </div>
-    )
-}

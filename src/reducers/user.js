59578bb6fc199d6c48ee85d552780ce877be9ef2
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-11 19:09:25 +0200

    for saving user in store(login) and deleted(logout)

---
+++ src/reducers/user.js
@@ -0,0 +1,31 @@
+import {
+    SET_USER,
+    LOGOUT
+} from '../constants'
+
+let initialState = {
+    currentUser: {},
+    isAuth: false
+}
+
+const user = (state = initialState, action) => {
+    switch (action.type) {
+        case SET_USER: 
+            return {
+                ...state,
+                currentUser: action.payload,
+                isAuth: true
+            }
+        case LOGOUT:
+            localStorage.removeItem('token')
+            return {
+                ...state,
+                currentUser: {},
+                isAuth: false
+            }
+        default:
+            return state
+    }
+}
+
+export default user

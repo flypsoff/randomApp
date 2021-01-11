cc024857fbdcd9aadbd70347032dc32ffcb6776b
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-11 19:24:31 +0200

    logic for login and showing error

---
+++ src/container/LoginContainer.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import { connect } from 'react-redux'
+import Login from '../components/auth/Login/Login'
+import { setUserThunk } from "../actions/users"
+
+const LoginContainer = (props) => {
+
+    const [errorMessage, setErrorMessage] = useState(null)
+
+    const handleSubmit = async (value) => {
+        const response = await props.onSetUser(value)
+        if(response) {
+            setErrorMessage(response)
+        }
+    }
+      
+    return (
+        <Login onSubmit={handleSubmit} errorMessage={errorMessage}/>
+    )
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    onSetUser: (user) => dispatch(setUserThunk(user))
+})
+
+export default connect(null, mapDispatchToProps)(LoginContainer)

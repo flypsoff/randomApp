70c1a86b810579e51be1b6335a77949eee4626e7
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-11 19:25:13 +0200

    logic for registration and showing error

---
+++ src/container/RegistrationContainer.js
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import { reset } from 'redux-form'
+import { registrationAPI } from '../api/api'
+import Registration from '../components/auth/Registration/Registration'
+
+const RegistrationContainer = (props) => {
+
+    const [errorMessage, setErrorMessage] = useState([])
+
+
+    const handleRegistration = async (value, dispatch) => {
+        try {
+
+            const result = await registrationAPI(value)
+            const message = result.data.message
+            alert(message)
+            dispatch(reset('registration'))
+        } catch (error) {
+            const errors = error.response.data.errors.errors
+            setErrorMessage(errors.map(item => item.msg))
+        }
+    }
+
+    return (
+        <Registration onSubmit={handleRegistration} errorMessage={errorMessage}/>
+    )
+}
+
+export default RegistrationContainer

4b01c0d943604966327bf5605845fdc0e4d853a1
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-11 19:13:26 +0200

    added api for authorization, login, registration

--- src/api/api.js
+++ src/api/api.js
@@ -8,11 +8,6 @@ export const getItemAPI = (path) => {
     return jsonAPI.get(path)
 }
 
-export const postContactUsAPI = (path, name, email, title, message) => {
-    return jsonAPI.post(path, {name, email, title, message})
-}
-
-
 const localAPI = axios.create({
     baseURL: 'http://localhost:3333/api/'
 })
@@ -21,7 +16,24 @@ export const getDetailAPI = (path) => {
     return localAPI.get(path)
 }
 
-
 export const getCarAPI = (id) => {
     return localAPI.get(`car/${id}`)
+}
+
+const authAPI = axios.create({
+    baseURL: 'http://localhost:3333/auth/'
+})
+
+export const registrationAPI = (body) => {
+    return authAPI.post('registration', body)
+}
+
+export const loginAPI = (body) => {
+    return authAPI.post('login', body)
+}
+
+export const authorizationAPI = () => {
+    return authAPI.get('authorization', {
+        headers:{Authorization: `Bearer ${localStorage.getItem('token')}`}
+    })
 }

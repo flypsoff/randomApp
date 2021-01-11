5bbdb5b79c67b0dea0bcb08920d190970da6ddd8
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-11 19:03:52 +0200

    used useSelector and useDispatch for auto authorization

--- src/App.js
+++ src/App.js
@@ -1,33 +1,25 @@
-import React from 'react'
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
 
 import Header from './components/Header/Header'
-import Home from './components/Home/Home'
-import History from './components/Todo/History/History'
+import Routers from './Routers'
 
-import TodoContainer from './container/TodoContainer'
-import CarContainer from './container/CarContainer'
-import CartContainer from './container/CartContainer'
-
-import withSuspense from './hoc/withSuspense'
-
-const ContactUsContainer = React.lazy(() => import('./container/ContactUsContainer'))
-const ShopContainer = React.lazy(() => import('./container/ShopContainer'))
+import { authorization } from './actions/users'
 
 function App() {
+  const isAuth = useSelector(state => state.user.isAuth) 
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(authorization())
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <BrowserRouter>
-      <Header />
-      <Switch>
-        <Route exact path={'/'} render={() => <Home />} />
-        <Route exact path={'/todo'} render={() => <TodoContainer />} />
-        <Route exact path={'/todo/history'} render={() => <History />} />
-        <Route exact path={'/car/:id?'} render={withSuspense(CarContainer)} />
-        <Route exact path={'/shop/cart'} render={() => <CartContainer />} />
-        <Route exact path={'/shop/:brand?'} render={withSuspense(ShopContainer)} />
-        <Route path={'/contactus'} render={withSuspense(ContactUsContainer)} />
-        <Redirect from={'/'} to={'/'} />
-      </Switch>
+      <Header isAuth={isAuth}/>
+      <Routers isAuth={isAuth}/>
     </BrowserRouter>
   )
 }

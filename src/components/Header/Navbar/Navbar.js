754bda9f9c27f57756afbc3d45c9e0211e153fbc
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-11 19:38:13 +0200

    changing route login-account depending on whether we logged in

--- src/components/Header/Navbar/Navbar.js
+++ src/components/Header/Navbar/Navbar.js
@@ -3,21 +3,31 @@ import styles from './Navbar.module.css'
 import { NavLink } from 'react-router-dom'
 
 
-const Navbar = () => {
+const Navbar = ({ isAuth }) => {
     return (
         <ul className={styles.navItems}>
             <NavLink to='/'>
                 <li>Home</li>
             </NavLink>
-            <NavLink to='/todo'>
-                <li>Todo</li>
-            </NavLink>
             <NavLink to='/shop'>
                 <li>Shop</li>
             </NavLink>
-            <NavLink to='/contactus'>
-                <li>Contact us</li>
+            <NavLink to='/todo'>
+                <li>Todo</li>
             </NavLink>
+            {
+                !isAuth &&
+                <NavLink to='/login'>
+                    <li>My account</li>
+                </NavLink>
+            }
+            {
+                isAuth &&
+                <NavLink to='/account'>
+                    <li>My account</li>
+                </NavLink>
+            }
+
         </ul>
     )
 }

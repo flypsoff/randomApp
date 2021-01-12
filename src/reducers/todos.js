d98356a81046ba898948adbea7e0a8ba97407394
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-12 17:39:17 +0200

    changed reducer for saving data on DB

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,77 +1,54 @@
 import { nanoid } from 'nanoid'
 import {
+    SET_CURRENT_TODOS,
+    CURRENT_TODO,
     ADD_TODO,
-    CHANGE_TODO_CHECKBOX,
-    CHANGE_CURRENT_TODO_VALUE,
-    DELETE_TODO,
-    DELETE_ALL_TODO
+    SET_COMPLETED_TODOS,
+    DELETE_ALL_TODOS
 } from './../constants'
 
 let initialTodosState = {
     currentTodoValue: '',
-    currentTodos: [
-        { title: 'Hello Kitty', completed: false, id: 10002 },
-        { title: 'Welcome to the party', completed: false, id: 10001 }
-    ],
-    deletedTodos: [
-        { title: 'My name is deleted object', completed: false, id: 10000 }
-    ]
+    currentTodos: [],
+    completedTodos: []
 }
 
 const todos = (state = initialTodosState, action) => {
     switch (action.type) {
         case ADD_TODO:
             let lastId = nanoid(5)
-
-            return ({
+            return {
                 ...state,
                 currentTodos: [
                     {
-                        title: action.title,
+                        title: action.todo,
                         completed: false,
                         id: lastId
                     },
                     ...state.currentTodos
                 ]
-            })
-            
-        case CHANGE_TODO_CHECKBOX:
-            let newState = state.currentTodos.map(item => {
-                if (item.id === action.idItem) {
-                    item.completed = !item.completed
-                }
-                return item
-            })
-            return ({
+            }
+        case CURRENT_TODO:
+            return {
                 ...state,
-                currentTodos: [
-                    ...newState
-                ]
-            })
-        case CHANGE_CURRENT_TODO_VALUE:
-            return ({
+                currentTodoValue: action.todo
+            }
+        case SET_CURRENT_TODOS:
+            return {
                 ...state,
-                currentTodoValue: action.currentValue
-            })
-        case DELETE_TODO:
-            let [ deletedTodo ] = state.currentTodos.filter(item => item.id === action.key)
-            return ({
+                currentTodos: [...action.current]
+            }
+        case SET_COMPLETED_TODOS:
+            return {
                 ...state,
-                currentTodos: [...state.currentTodos.filter(item => item.id !== deletedTodo.id)],
-                deletedTodos: [
-                    deletedTodo,
-                    ...state.deletedTodos
-                ]
-            })
-        case DELETE_ALL_TODO: 
-            return ({
-                ...state, 
+                completedTodos: [...action.completed]
+            }
+        case DELETE_ALL_TODOS: 
+            return {
+                ...state,
                 currentTodos: [],
-                deletedTodos: [
-                    ...state.currentTodos,
-                    ...state.deletedTodos
-                ]
-            })
+                completedTodos: []
+            }
         default:
             return state
     }

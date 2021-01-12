4ebc4073d5898be54615e3d761948ce65cab3e2d
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-12 17:41:23 +0200

    some refactoring

--- src/actions/users.js
+++ src/actions/users.js
@@ -5,10 +5,10 @@ import {
 
 import {
     authorizationAPI,
-    loginAPI 
+    loginAPI
 } from './../api/api'
 
-export const setUser = (user) => ({type: SET_USER, payload: user})
+export const setUser = (user) => ({ type: SET_USER, payload: user })
 export const setUserThunk = (user) => async dispatch => {
     try {
         const result = await loginAPI(user)
@@ -19,17 +19,15 @@ export const setUserThunk = (user) => async dispatch => {
     }
 }
 
-export const logout = () => ({type: LOGOUT})
+export const logout = () => ({ type: LOGOUT })
 
-export const authorization = () => {
-    return async dispatch => {
-        try {
-            const response = await authorizationAPI()
-            dispatch(setUser(response.data.user))
-            localStorage.setItem('token', response.data.token)
-        } catch (e) {   
-            console.log(e.response.data.message)
-            localStorage.removeItem('token')
-        }
+export const authorization = () => async dispatch => {
+    try {
+        const response = await authorizationAPI()
+        dispatch(setUser(response.data.user))
+        localStorage.setItem('token', response.data.token)
+    } catch (e) {
+        console.log(e.response.data.message)
+        localStorage.removeItem('token')
     }
 }

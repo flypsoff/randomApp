09be9b338b56f3c5f1c3f4550c632dbffefa0cec
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-13 18:15:23 +0200

    no we will not use email, cause we have TOKEN)

--- src/actions/todos.js
+++ src/actions/todos.js
@@ -1,38 +1,58 @@
-import {
-    SET_CURRENT_TODOS,
-    CURRENT_TODO,
-    ADD_TODO,
-    SET_COMPLETED_TODOS,
-    DELETE_ALL_TODOS
-} from './../constants'
+import * as types from './../constants'
+import * as api from './../api/todosAPI'
 
-import {
-    getCurrentTodosAPI,
-    getCompletedTodosAPI
-} from './../api/todosAPI'
+export const setCurrentTodos = (current) => ({type: types.SET_CURRENT_TODOS, current})
+export const setCompletedTodos = (completed) => ({type: types.SET_COMPLETED_TODOS, completed})
 
-export const setCurrentTodos = (current) => ({type: SET_CURRENT_TODOS, current})
-export const setCompletedTodos = (completed) => ({type: SET_COMPLETED_TODOS, completed})
+export const currentTodo = (todo) => ({type: types.CURRENT_TODO, todo})
 
-export const currentTodo = (todo) => ({type: CURRENT_TODO, todo})
-export const addTodo = (todo) => ({type: ADD_TODO, todo})
-
-export const setCurrentTodosThunk = (email) => async dispatch => {
+export const addCurrentTodo = (todo) => ({type: types.ADD_CURRENT_TODO, todo})
+export const addCurrentTodoThunk = (currentTodo) => async dispatch => {
     try {
-        const res = await getCurrentTodosAPI(email)
+        const res = await api.addCurrentTodosAPI(currentTodo)
+        const todo = res.data.todo
+        dispatch(addCurrentTodo(todo))
+    } catch (e) {
+        console.log(e.response.data.message)
+    }
+}
+
+export const addCompletedTodos = (deleted, newCurrentTodos) => ({type: types.ADD_COMPLETED_TODO, deleted, newCurrentTodos})
+export const addCompletedTodoThunk = (id) => async dispatch => {
+    try {
+        const res = await api.addCompletedTodosAPI(id)
+        dispatch(addCompletedTodos(res.data.deleted, res.data.newCurrentTodos))
+    } catch (e) {
+        console.log(e.response.data.message)
+    }
+}
+
+export const setCurrentTodosThunk = () => async dispatch => {
+    try {
+        const res = await api.getCurrentTodosAPI()
         dispatch(setCurrentTodos(res.data.todos))
     } catch (e) {
         console.log(e.response.data.message)
     }
 }
 
-export const setCompletedTodosThunk = (email) => async dispatch => {
+export const setCompletedTodosThunk = () => async dispatch => {
     try {
-        const res = await getCompletedTodosAPI(email)
+        const res = await api.getCompletedTodosAPI()
         dispatch(setCompletedTodos(res.data.todos))
     } catch (e) {
         console.log(e.response.data.message)
     }
 }
 
-export const deleteAllTodos = () => ({type: DELETE_ALL_TODOS})
+export const deleteAllTodos = () => ({type: types.DELETE_ALL_TODOS})
+
+export const deleteCompletedTodos = (newCompletedTodos) => ({type: types.DELETE_COMPLETED_TODO, newCompletedTodos})
+export const deleteCompletedTodosThunk = (id) => async dispatch => {
+    try {
+        const res = await api.deleteCompletedTodosAPI(id)
+        dispatch(deleteCompletedTodos(res.data.newCompleted))
+    } catch (e) {
+        console.log(e.response.data.message)
+    }
+}

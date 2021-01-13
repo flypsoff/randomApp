f52cae981a722526cc9eb815021702f13c8ae2d2
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-13 18:11:48 +0200

    if we not logged in we can not go to /todo/... . Import was refactored

--- src/Routers.js
+++ src/Routers.js
@@ -1,36 +1,40 @@
-import React from "react";
+import React from 'react'
 import { Route, Switch, Redirect } from 'react-router-dom'
+
+import withSuspense from './hoc/withSuspense'
 
 import Home from './components/Home/Home'
 
-import {
-    CarContainer,
-    CartContainer,
-    RegistrationContainer,
-    LoginContainer,
-    AccountContainer
-} from "./container";
-
-import withSuspense from './hoc/withSuspense'
+import * as container from './container'
 
 const ShopContainer = React.lazy(() => import('./container/ShopContainer'))
 const TodoContainer = React.lazy(() => import('./container/TodoContainer'))
 const CompletedContainer = React.lazy(() => import('./container/CompletedContainer'))
 
-
-
 const Routers = (props) => {
     return (
         <Switch>
             <Route exact path={'/'} render={() => <Home />} />
-            <Route exact path={'/todo/current'} render={withSuspense(TodoContainer)} />
-            <Route exact path={'/todo/completed'} render={withSuspense(CompletedContainer)} />
-            <Route exact path={'/car/:id?'} render={withSuspense(CarContainer)} />
-            <Route exact path={'/shop/cart'} render={() => <CartContainer />} />
+            { props.isAuth 
+                ? <Route exact path={'/todo/current'} render={withSuspense(TodoContainer)} />
+                : <Redirect from={'/todo/current'} to={'/login'} />
+            }
+            { props.isAuth
+                ? <Route exact path={'/todo/completed'} render={withSuspense(CompletedContainer)} />
+                : <Redirect from={'/todo/completed'} to={'/login'} />
+            }
+            { props.isAuth
+                ? <Route exact path={'/account'} render={() => <container.AccountContainer />} />
+                : <Redirect from={'/account'} to={'/login'} />
+            }
+
+            {!props.isAuth && <Route exact path={'/login'} render={() => <container.LoginContainer />} />}
+            {!props.isAuth && <Route exact path={'/registration'} render={() => <container.RegistrationContainer />} />}
+
+            <Route exact path={'/car/:id?'} render={withSuspense(container.CarContainer)} />
+            <Route exact path={'/shop/cart'} render={() => <container.CartContainer />} />
             <Route exact path={'/shop/:brand?'} render={withSuspense(ShopContainer)} />
-            {!props.isAuth && <Route exact path={'/login'} render={() => <LoginContainer />} />}
-            {!props.isAuth && <Route exact path={'/registration'} render={() => <RegistrationContainer />} />}
-            {props.isAuth && <Route exact path={'/account'} render={() => <AccountContainer />} />}
+            
             <Redirect from={'/'} to={'/'} />
         </Switch>
     )

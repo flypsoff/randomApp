9330165285050873e65cacc76dc24ef12caedf57
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-13 17:55:53 +0200

    special component for buttons on todo page

---
+++ src/components/Todo/Cases/Cases.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import styles from './Cases.module.css'
+
+
+const Cases = (props) => {
+    return (
+        <div className={styles.casesContainer}>
+            <NavLink to={`/todo/completed`}>
+                <button>To completed</button>
+            </NavLink>
+            <button >Complete all</button>
+        </div>
+    )
+}
+
+export default Cases

bef65f0263f48da75ff55af8865b3b054e34c494
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-13 18:13:36 +0200

    nothing

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers  } from 'redux'
+import { reducer as formReducer } from 'redux-form'
+
 import todos from './todos'
 import shops from './shops'
 import user from './user'
-import { reducer as formReducer } from 'redux-form'
 
 const reducers = combineReducers({
     todos,

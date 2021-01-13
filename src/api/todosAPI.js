7d280527997b5864204b7f489c53919689cd7a21
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-13 18:15:39 +0200

    no we will not use email, cause we have TOKEN)

--- src/api/todosAPI.js
+++ src/api/todosAPI.js
@@ -1,13 +1,35 @@
 import axios from 'axios'
+import { nanoid } from 'nanoid'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333/todos'
+    baseURL: 'http://localhost:3333/todos',
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+    }
 })
 
-export const getCurrentTodosAPI = (email) => {
-    return api.post('/current', {email})
+export const getCurrentTodosAPI = () => {
+    return api.get('/current')
 }
 
-export const getCompletedTodosAPI = (email) => {
-    return api.post('/completed', {email})
+export const getCompletedTodosAPI = () => {
+    return api.get('/completed')
+}
+
+export const addCurrentTodosAPI = (currentTodo) => {
+    let lastId = nanoid(5)
+    const todo = {
+        title: currentTodo,
+        completed: false,
+        id: lastId
+    }
+    return api.patch('/current/add', {todo})
+}
+
+export const addCompletedTodosAPI = (id) => {
+    return api.patch('/completed/add', {id})
+}
+
+export const deleteCompletedTodosAPI = (id) => {
+    return api.patch('/completed/delete', {id})
 }

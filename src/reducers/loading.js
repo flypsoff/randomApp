6473d543b4c5346429cc8221d502c2ae8fd3b44e
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-18 20:28:18 +0200

    i goint to use it for disabled for exm. button is loading

---
+++ src/reducers/loading.js
@@ -0,0 +1,27 @@
+import {
+    START_LOADING,
+    END_LOADING
+} from '../constants'
+
+let initialState = {
+    isLoading: falst
+}
+
+const loading = (state = initialState, action) => {
+    switch (action.type) {
+        case START_LOADING: 
+            return {
+                ...state,
+                isLoading: true
+            }
+        case END_LOADING: 
+            return {
+                ...state,
+                isLoading: false
+            }
+        default:
+            return state
+    }
+}
+
+export default loading

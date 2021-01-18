fbcdcfc20ef0e77819ccff5586b211e39b4dd29a
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-18 20:32:12 +0200

    created deleted container for creating logic for deleted todos

---
+++ src/container/DeletedContainer.js
@@ -0,0 +1,41 @@
+import { useEffect } from 'react'
+import { connect } from 'react-redux'
+
+import { setDeletedTodosThunk, deleteDeletedTodosThunk } from '../actions/todos'
+import LogginWarn from '../components/commons/LoginWarn/LoginWarn'
+import Completed from '../components/Todo/Deleted/Deleted'
+
+const CompletedContainer = (props) => {
+
+    useEffect(() => {
+        if (props.isAuth) {
+            props.onSetDeletedTodos()
+        }
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [props.isAuth])
+
+    if (!props.isAuth) {
+        return (
+            <LogginWarn>
+                Sorry, but if you want to see deleted todos you need to login!
+            </LogginWarn>
+        )
+    }
+
+    return (
+        <Completed {...props} />
+    )
+}
+
+const mapStateToProps = (state) => ({
+    isAuth: state.user.isAuth,
+    deletedTodos: state.todos.deletedTodos
+})
+
+const mapDispatchToProps = (dispatch) => ({
+    onSetDeletedTodos: () => dispatch(setDeletedTodosThunk()),
+    onDeleteDeletedTodos: (id) => dispatch(deleteDeletedTodosThunk(id))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CompletedContainer)

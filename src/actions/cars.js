e5bfcc35e734918fa9509cde009224fd25eae0d3
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-20 17:19:54 +0200

    for saving cars, brands, and filters. Last one for sending req with search params

---
+++ src/actions/cars.js
@@ -0,0 +1,35 @@
+import {
+    GET_CARS,
+    GET_BRANDS,
+    SET_FILTER
+} from './../constants'
+
+import {
+    getCarsAPI,
+    getBrandsAPI
+} from './../api/carsAPI'
+
+export const setFilter = (payload) => ({ type: SET_FILTER, payload})
+
+export const getCars = (payload) => ({ type: GET_CARS, payload })
+export const getCarsThunk = (filter) => async (dispatch) => {
+    try {
+        dispatch(setFilter(filter))
+        const res = await getCarsAPI(filter)
+        dispatch(getCars(res.data.cars))
+    } catch (e) {
+        console.log(e.response.data.message)
+    }
+
+}
+
+export const getBrands = (payload) => ({ type: GET_BRANDS, payload })
+export const getBrandsThunk = () => async (dispatch) => {
+    try {
+        const res = await getBrandsAPI()
+        dispatch(getBrands(res.data.brands))
+    } catch (e) {
+        console.log(e.response.data.message)
+    }
+
+}
---
+++ src/reducers/cars.js
@@ -0,0 +1,43 @@
+import {
+    GET_CARS,
+    GET_BRANDS,
+    SET_FILTER
+} from '../constants'
+
+let initialState = {
+    cars: [],
+    brands: [],
+    filter: {
+        brand: '',
+        bodyType: '',
+        carClass: '',
+        fromYear: '',
+        toYear: '',
+        fromPrice: '',
+        toPrice: ''
+    }
+}
+
+const cars = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_CARS:
+            return {
+                ...state,
+                cars: action.payload
+            }
+        case GET_BRANDS:
+            return {
+                ...state,
+                brands: action.payload
+            }
+        case SET_FILTER:
+            return {
+                ...state,
+                filter: action.payload
+            }
+        default:
+            return state
+    }
+}
+
+export default cars

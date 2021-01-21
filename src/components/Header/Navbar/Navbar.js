eddbaa94c298449cde0f5e44a139050e51c9cf65
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-21 15:15:01 +0200

    added onclick to cars navlink for reset filter

--- src/components/Header/Navbar/Navbar.js
+++ src/components/Header/Navbar/Navbar.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import styles from './Navbar.module.css'
 import { NavLink } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { resetFilter } from '../../../actions/cars'
 
 
 const Navbar = ({ isAuth }) => {
+
+    const dispatch = useDispatch()
+    const filterRef = useRef(useSelector(state => state.cars.filter))
+    
+    const handleClick = () => {
+        dispatch(resetFilter(filterRef.current))
+    }
+
     return (
         <ul className={styles.navItems}>
             <NavLink to='/'>
                 <li>Home</li>
             </NavLink>
-            <NavLink to='/cars'>
+            <NavLink to='/cars' onClick={handleClick}>
                 <li>Cars</li>
             </NavLink>
             <NavLink to='/todos'>

1003e618dde8e58d5b9e4cc4324215df020efd43
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-21 15:12:17 +0200

    we created 2 useEffect for checking our URL and fiil in our filter

--- src/components/Cars/CarsCollection/CarsCollection.js
+++ src/components/Cars/CarsCollection/CarsCollection.js
@@ -1,23 +1,37 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { useHistory } from 'react-router-dom'
 import { getCarsThunk } from '../../../actions/cars'
 import styles from './CarsCollection.module.css'
+import queryString from 'query-string'
+import getSearchParams from '../../../utils/getSearchParams'
 
 const CarsCollection = (props) => {
     const dispatch = useDispatch()
     const cars = useSelector(state => state.cars.cars)
     const filter = useSelector(state => state.cars.filter)
+    const history = useHistory()
 
     useEffect(() => {
-        dispatch(getCarsThunk(filter))
+        const parsed = queryString.parse(history.location.search)
+        const actualFilter = { ...filter, ...parsed }
+        dispatch(getCarsThunk(actualFilter))
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [dispatch])
+    }, [history.location.search])
+
+    useEffect(() => {
+        history.push({
+            pathname: '/cars',
+            search: getSearchParams(filter)
+        })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [filter])
 
     return (
         <div className={styles.carsCollectionContainer}>
             {cars && cars.map(car => (
                 <div key={car.carID} className={styles.carsItem}>
-                    <img src={car.img} alt={car.brand} width='200px' height='150px'/>
+                    <img src={car.img} alt={car.brand} width='300px' height='200px' />
                     <div className={styles.carsInfo}>
                         <h3>Brand: {car.brand} {car.model}</h3>
                         <h4>Class: {car.class}</h4>

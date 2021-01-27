621d731275d9f8fcbe81772f9a762fb424ac432a
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-27 14:16:57 +0200

    if we authorizated we can add car

--- src/components/Cars/Cars.js
+++ src/components/Cars/Cars.js
@@ -2,15 +2,30 @@ import React from 'react'
 import CarsCollection from './CarsCollection/CarsCollection'
 import Filter from './Filter/Filter'
 import styles from './Cars.module.css'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 const Cars = (props) => {
+    const isAuth = useSelector(state => state.user.isAuth)
+
     return (
-        <div className={styles.carsContainer}>
-            <div className={styles.filterContainer}>
-                <Filter onFilterChanged={props.onFilterChanged} handleReset={props.handleReset}/>
-            </div>
-            <div className={styles.carsCollectionContainer}>
-                <CarsCollection />
+        <div>
+            {isAuth &&
+                <div className={styles.addCarContainer}>
+
+                    <Link to='/car/addcar'>
+                        <b>You can add you own car and other people will see it. But no more than <span>THREE</span></b>
+                    </Link>
+                </div>
+            }            
+
+            <div className={styles.carsContainer}>
+                <div className={styles.filterContainer}>
+                    <Filter onFilterChanged={props.onFilterChanged} handleReset={props.handleReset} />
+                </div>
+                <div className={styles.carsCollectionContainer}>
+                    <CarsCollection />
+                </div>
             </div>
         </div>
     )

e532ba4f386468db9830285d90228148c42ea871
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-30 11:48:58 +0200

    nothing changes

--- src/container/OneCarContainer.js
+++ src/container/OneCarContainer.js
@@ -8,6 +8,7 @@ const OneCarContainer = (props) => {
     const {pathname} = useLocation()
     const dispatch = useDispatch()
     const car = useSelector(state => state.cars.currentCar)
+
     useEffect(() => {
         dispatch(getCarByIdThunk(pathname))
         // eslint-disable-next-line react-hooks/exhaustive-deps

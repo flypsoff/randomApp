de976b8de288b0c2a2aa44ca36510bf650a20bc3
Vlad Sevostianov <flypsoff@gmail.com>
2021-01-30 11:47:43 +0200

    logic for adding car and checking if we logged in

--- src/container/AddCarContainer.js
+++ src/container/AddCarContainer.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { addCarAPI } from '../api/carsAPI'
 import AddCar from '../components/Cars/AddCar/AddCar'
+import LogginWarn from '../components/commons/LoginWarn/LoginWarn'
 
 const AddCarContainer = (props) => {
+    const isAuth = useSelector(state => state.user.isAuth)
 
-    const handleSubmit = (values) => {
-        console.log(values)
+    const handleSubmit = async (values) => {
+        try {
+            const res = await addCarAPI(values)
+            alert(res.data.message)
+        } catch (e) {
+            console.log(e.response.data.message)
+        }  
+    }
+
+    if (!isAuth) {
+        return (
+            <LogginWarn>
+                Sorry, but if you want post your car you need to login!
+            </LogginWarn>
+        )
     }
     
     return (

c82dd15f0596683f3f517860be989316506faf57
Vlad Sevostianov <flypsoff@gmail.com>
2021-02-03 13:06:39 +0200

    test

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 const Home = () => {
+    const [count, setCount] = useState(0)
+    const prevCount = useRef()
 
+    useEffect(() => {
+        prevCount.current = count
+    })
+    const prev = prevCount.current
     return (
-        <h1>Hello</h1>
+        <>
+        <h1>Now: {count}, prev: {prev}</h1>
+        <button onClick={() => setCount(count + 1 )}>click</button>
+        </>
     )
 }
 

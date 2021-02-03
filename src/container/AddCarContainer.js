0127ecb5e8a65c31570cda84d210b9ae2ee76ce9
Vlad Sevostianov <flypsoff@gmail.com>
2021-02-03 13:08:47 +0200

    dispatching addPost for added to posts array

--- src/container/AddCarContainer.js
+++ src/container/AddCarContainer.js
@@ -1,15 +1,18 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { addPost } from '../actions/account'
 import { addCarAPI } from '../api/carsAPI'
 import AddCar from '../components/Cars/AddCar/AddCar'
 import LogginWarn from '../components/commons/LoginWarn/LoginWarn'
 
 const AddCarContainer = (props) => {
     const isAuth = useSelector(state => state.user.isAuth)
+    const dispatch = useDispatch()
 
     const handleSubmit = async (values) => {
         try {
             const res = await addCarAPI(values)
+            dispatch(addPost(res.data.post))
             alert(res.data.message)
         } catch (e) {
             console.log(e.response.data.message)

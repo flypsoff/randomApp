5a82fe0f25d4e37f9bebaa4701373d11e10422d0
Vlad Sevostianov <flypsoff@gmail.com>
2021-02-04 15:46:14 +0200

    deleting post(car)

--- src/container/AccountContainer.js
+++ src/container/AccountContainer.js
@@ -2,17 +2,21 @@ import React from "react";
 import { connect } from "react-redux";
 import { logout } from "../actions/users";
 import { logoutTodos } from '../actions/todos'
-import { changeFieldThunk, changeEmailThunk, changePasswordThunk } from "../actions/account";
+import { changeFieldThunk, changeEmailThunk, changePasswordThunk, deletePostThunk } from "../actions/account";
 import Account from '../components/auth/Account/Account'
 
-const AccountContainer = ({onLogout, onLogoutTodos, ...props}) => {
+const AccountContainer = ({onLogout, onLogoutTodos, onDeletePost, ...props}) => {
     const handleLogout = () => {
         onLogout()
         onLogoutTodos()
     }
 
+    const handleDelete = async (carID) => {
+        onDeletePost(carID)
+    }
+
     return (
-        <Account handleLogout={handleLogout} {...props}/>
+        <Account handleLogout={handleLogout} handleDelete={handleDelete} {...props}/>
     )
 }
 
@@ -26,7 +30,8 @@ const mapDispatchToProps = (dispatch) => ({
     onLogoutTodos: () => dispatch(logoutTodos()),
     onChangeField: (field, state) => dispatch(changeFieldThunk(field, state)),
     onChangeEmail: (field, email, pass) => dispatch(changeEmailThunk(field, email, pass)),
-    onChangePassword: (field, email, pass) => dispatch(changePasswordThunk(field, email, pass))
+    onChangePassword: (field, email, pass) => dispatch(changePasswordThunk(field, email, pass)),
+    onDeletePost: (carID) => dispatch(deletePostThunk(carID))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(AccountContainer)
